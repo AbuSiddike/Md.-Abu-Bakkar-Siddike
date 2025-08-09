c3c172f8da0eaafc5951f3299654e84939e5e433
AbuSiddike <abubakkarsiddike1997@gmail.com>
2025-08-09 23:45:15 +0600

    home page updated with new components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Hero from "../components/Hero";
+import AboutMe from "../components/AboutMe";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
+import Resume from "../components/Resume";
+import Contact from "../components/Contact";
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <>
+            <Hero />
+            <AboutMe />
+            <Projects />
+            <Skills />
+            <Resume />
+            <Contact />
+        </>
+    );
 }
 
-export default Home
+export default Home;
