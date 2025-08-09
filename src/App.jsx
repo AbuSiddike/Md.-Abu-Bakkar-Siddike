419da815816b253966960465cc12c348625a64f4
AbuSiddike <abubakkarsiddike1997@gmail.com>
2025-08-09 21:27:09 +0600

    main.jsx and app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import './index.css'
+import { Routes, Route } from "react-router-dom"
+import Layout from "./components/Layout"
+import Home from "./pages/Home"
+import ProjectDetails from "./pages/ProjectDetails"
 
 function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+    
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="projects/:id" element={<ProjectDetails />} />
+            </Route>
+        </Routes>
+    );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router";
-import { Slide, ToastContainer } from "react-toastify";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-    },
-]);
-
-createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick={false}
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-            transition={Slide}
-        />
-        <App />
-        <RouterProvider router={router} />
-    </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
